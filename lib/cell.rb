5adc8ac642756b2b09b83f60a0cd30a327ae2605
Neil Atkinson <nosniktatkinson@hotmail.co.uk>
2013-06-06 12:50:29 +0100

    changing 'possible_values' to jsut 'possibiliities'

--- lib/cell.rb
+++ lib/cell.rb
@@ -1,28 +1,29 @@
 
 class Cell
 include Comparable
-attr_accessor :possible_values
-attr_reader :index
+attr_accessor :possibilities
+attr_reader :position
 
-def initialize(value)
+def initialize(value, position)
   raise ArgumentError unless value.is_a?(String)
-  @possible_values = (value == "0") ? ["1","2","3","4","5","6","7","8","9"] : [value.to_s]
+  @possibilities = (value == "0") ? ["1","2","3","4","5","6","7","8","9"] : [value.to_s]
+  @position = position
 end
 
-def subtract_from_possible_values(impossible_values)
-  self.possible_values = possible_values - impossible_values
+def subtract_from_possibilities(impossibilities)
+  self.possibilities = possibilities - impossibilities
 end
 
 def finalized?
-  possible_values.count == 1
+  possibilities.count == 1
 end
 
 def to_s
-  finalized? ? possible_values.first : "0"
+  finalized? ? possibilities.first : "0"
 end
 
 def <=>(other_cell)
-  self.possible_values <=> other_cell.possible_values
+  self.possibilities <=> other_cell.possibilities
 end
 
 end
--- test/cell_test.rb
+++ test/cell_test.rb
@@ -5,67 +5,72 @@ require '../lib/cell'
 
 describe Cell do
   before do
-    @finalized_cell = Cell.new("2")
-    @non_finalized_cell = Cell.new("0")
+    @finalized_cell = Cell.new("2", 0)
+    @non_finalized_cell = Cell.new("0", 1)
+  end
+
+  it "should know its position" do
+    @finalized_cell.position.must_equal 0
+    @non_finalized_cell.position.must_equal 1
   end
 
   it "should have possible values" do
-    @finalized_cell.must_respond_to :possible_values
-    @finalized_cell.must_respond_to :possible_values=
-    @finalized_cell.possible_values.must_be_instance_of Array
+    @finalized_cell.must_respond_to :possibilities
+    @finalized_cell.must_respond_to :possibilities=
+    @finalized_cell.possibilities.must_be_instance_of Array
   end
 
   it "should have an array of possible values from 1 to 9 when the initialized value is 0" do
-    @non_finalized_cell.possible_values.must_equal ["1","2","3","4","5","6","7","8","9"]
+    @non_finalized_cell.possibilities.must_equal ["1","2","3","4","5","6","7","8","9"]
   end
 
-  describe "subtract_from_possible_values method" do
-    it "should accept an array of numbers and remove the numbers in those arrays from the array of possible_values" do
+  describe "subtract_from_possibilities method" do
+    it "should accept an array of numbers and remove the numbers in those arrays from the array of possibilities" do
       array = ["1","2","3","4","4","5","6"]
-      @non_finalized_cell.subtract_from_possible_values(array).must_equal ["7","8","9"]
-      @non_finalized_cell.possible_values.must_equal ["7","8","9"]
+      @non_finalized_cell.subtract_from_possibilities(array).must_equal ["7","8","9"]
+      @non_finalized_cell.possibilities.must_equal ["7","8","9"]
     end
   end
 
   it "should be finalized if it has only one possible value" do
-    @finalized_cell.possible_values.count.must_equal 1
+    @finalized_cell.possibilities.count.must_equal 1
     @finalized_cell.finalized?.must_equal true
   end
 
   it "should have a non-zero value when finalized" do
-    @non_finalized_cell.subtract_from_possible_values(["1","2","3","4","5","6","7","8"])
+    @non_finalized_cell.subtract_from_possibilities(["1","2","3","4","5","6","7","8"])
     @non_finalized_cell.finalized?.must_equal true
-    @non_finalized_cell.possible_values.count.must_equal 1
-    @non_finalized_cell.possible_values.must_equal ["9"]
+    @non_finalized_cell.possibilities.count.must_equal 1
+    @non_finalized_cell.possibilities.must_equal ["9"]
   end
 
   it "should not be finalized if it has more than one possible value" do
-    @non_finalized_cell.possible_values.count.must_equal 9
-    @non_finalized_cell.possible_values.must_equal ["1","2","3","4","5","6","7","8","9"]
+    @non_finalized_cell.possibilities.count.must_equal 9
+    @non_finalized_cell.possibilities.must_equal ["1","2","3","4","5","6","7","8","9"]
     @non_finalized_cell.finalized?.must_equal false
   end
 
   describe "to_s method" do
     it "should return a string containing the cell's last possible value finalized" do
-      @finalized_cell.possible_values.count.must_equal 1
+      @finalized_cell.possibilities.count.must_equal 1
       @finalized_cell.finalized?.must_equal true
       @finalized_cell.to_s.must_equal "2"
     end
 
-    it "should return '0' when there are multiple possible_values" do
+    it "should return '0' when there are multiple possibilities" do
       @non_finalized_cell.to_s.must_equal "0"
     end
   end
 
   describe "equality" do
     it "should know when two cells are equal" do
-      @non_finalized_cell.must_equal Cell.new("0")
-      @finalized_cell.must_equal Cell.new("2")
+      @non_finalized_cell.must_equal Cell.new("0",1)
+      @finalized_cell.must_equal Cell.new("2",0)
     end
 
     it "should know when two cells are not equal" do
-      @non_finalized_cell.wont_equal Cell.new("2")
-      @finalized_cell.wont_equal Cell.new("0")
+      @non_finalized_cell.wont_equal Cell.new("2",0)
+      @finalized_cell.wont_equal Cell.new("0",1)
     end
   end
 
